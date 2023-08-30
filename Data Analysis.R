2c3ce1fe503f3f45a1dac99eabd675d3b467e973
logaprasath1801 <142376223+logaprasath1801@users.noreply.github.com>
2023-08-30 09:42:18 +0530

    Data Analysis.R

--- Data Analysis.R
+++ Data Analysis.R
@@ -4,7 +4,7 @@ library(lubridate) #dates
 library(hms) #time
 library(data.table) #exporting data frame
 
-#load original .csv files, a years worth of data from August 2020 to July 2021
+#load original .csv files, a years worth of data from January 2022 to December 2022
 jan01_df <- read_csv("202201-divvy-tripdata.csv") 
 feb02_df <- read_csv("202202-divvy-tripdata.csv") 
 mar03_df <- read_csv("202203-divvy-tripdata.csv")
